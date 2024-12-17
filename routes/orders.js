5a711cad697a676f8999edc671dab58bea0c7cdd
matterattetatte <matias.marti@medieinstitutet.se>
2024-12-17 18:04:15 +0100

    make sure to reduce stock for correct size

--- routes/orders.js
+++ routes/orders.js
@@ -1,14 +1,29 @@
 const express = require('express');
 const router = express.Router();
+const Products = require('../models/Product');
 const Order = require('../models/Order');
 
 router.post('/', async (req, res) => {
     try {
-        res.json(await new Order(req.body).save());
-    } catch(error) {
-        res.json({message: error});
+        const productIds = req.body.cartData.map((data) => data.productId)
+        const productsInDb = await Products.find({ _id: { $in: productIds } })
+
+        productsInDb.forEach(p => {
+            const cartDataEntry = req.body.cartData.find((data) => data.productId === p._id.toString())
+            const foundStockIdx = p.stock.findIndex((s) => s.size === cartDataEntry.size)
+
+            p.stock[foundStockIdx].quantity -= 1
+
+            p.markModified('stock')
+            p.save()
+        })
+
+        res.json(await new Order(req.body).save())
+    } catch (error) {
+        console.log('error', error)
+        res.json({message: error})
     }
 });
 
 
-module.exports = router;
+module.exports = router
